b4e8bc5bdc500feace734cfc36bfcc19d98e0f8d
s-vish <svishal077@gmail.com>
2020-06-25 13:50:47 +0530

    Dropdown code enhanced

--- src/main/java/com/selenium/automation/Dropdown.java
+++ src/main/java/com/selenium/automation/Dropdown.java
@@ -1,8 +1,11 @@
 package com.selenium.automation;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterTest;
@@ -26,16 +29,23 @@ public class Dropdown {
 		year=new Select(driver.findElement(By.xpath("//select[@title='Year' and @name='birthday_year']")));
 		
 		try {
-		day.selectByValue("10");
+		day.selectByIndex(10); // will select the value present at the index 10
 		
-		month.selectByIndex(12);
+		month.selectByValue("12"); // will select the value present at the value 12
 		
-		year.selectByValue("1990");
+		year.selectByVisibleText("1990"); //will look for the text provided and select it
 		}
 		catch (NoSuchElementException e) {
 			// TODO: handle exception
 			e.printStackTrace();
 //			System.out.println(e);
+		}
+		
+		
+		//print all the dropdown values from days dropdown
+		List<WebElement> lst = day.getOptions();
+		for (WebElement webElement : lst) {
+			System.out.println(webElement.getText());
 		}
 	}
 	@AfterTest
